49ae695b921d35e31d045d3de4bdaeaee8e53eed
Durvankur Rajam <durvankurrajam099@gmail.com>
2026-01-26 22:42:45 +0530

    Fix CV link filename in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -22,7 +22,7 @@ const ContactPage = () => {
     },
     {
       name: "CV",
-      link: "durvankur_cv.pdf",
+      link: "durvankur_CV.pdf",
       description: "Download my resume",
     },
   ];
